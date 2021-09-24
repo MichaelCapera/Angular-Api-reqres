c19ec3054c5c109c7a587ab0109159f92555fbb3
Michael Capera <michaelcapera@gmail.com>
2021-09-24 10:22:14 -0500

    login and main head

--- src/app/feature/users/users.module.ts
+++ src/app/feature/users/users.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeUserComponent } from './home-user/home-user.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 @NgModule({
     declarations: [
        HomeUserComponent,
+       NavBarComponent
       ],
 
 })
